24b90a787f91a8745ac370910963b02562e7e1b4
Fabian Kunkler <fabian.kunkler@alpha-sys.de>
2015-02-16 10:57:02 +0100

    new files for oxide 4.9

--- oxid_greater_4.9/copy_this/modules/asy/asy_mantext/metadata.php
+++ oxid_greater_4.9/copy_this/modules/asy/asy_mantext/metadata.php
@@ -1,37 +1,36 @@
 <?php
 /**
  * @web http://www.alpha-sys.de
- * @copyright Alpha-Sys 2012
+ * @copyright Alpha-Sys 2008-2014
  * @author Fabian Kunkler 
  * @sponsored from sunlab gmbh, http://www.sunlab.de
  * @email fabian.kunkler@alpha-sys.de
- * @version 1.0 16:00:00 25.11.2012
+ * @version 1.4 16:00:00 13.11.2014
  */
  
 /**
  * Metadata version
  */
-$sMetadataVersion = '1.0';
+$sMetadataVersion = '1.1';
 
 /**
  * Module information
  */
 $aModule = array(
     'id'           => 'asy_mantext',
-    'title'        => 'Hersteller-Beschreibung',
+    'title'        => '<img src="../modules/asy/module_logo.png" alt="Alpha-Sys" title="Alpha-Sys"> Hersteller-Beschreibung',
     'description'  => 'Beschreibung für den Hersteller',
-    'thumbnail'    => '',
-    'version'      => '1.2',
+    'thumbnail'    => 'module_logo.png',
+    'version'      => '1.4',
     'author'       => 'Alpha-Sys',
     'sponsor'      => 'Sunlab GmbH',
     'url'          => 'http://www.alpha-sys.de',
     'email'        => 'fabian.kunkler@alpha-sys.de',
 
     'extend'       => array(
-        'manufacturer_main'      => 'asy_mantext/admin/asy_mantext__manufacturer_main',
-        'manufacturerlist'       => 'asy_mantext/views/asy_mantext__manufacturerlist',
-        'oxmanufacturer'         => 'asy_mantext/core/asy_mantext__oxmanufacturer',
-        'oxmanufacturerlist'     => 'asy_mantext/core/asy_mantext__oxmanufacturerlist',
+        'manufacturer_main'      => 'asy/asy_mantext/controllers/admin/asy_mantext__manufacturer_main',
+        'manufacturerlist'       => 'asy/asy_mantext/controllers/asy_mantext__manufacturerlist',
+        'oxmanufacturer'         => 'asy/asy_mantext/models/asy_mantext__oxmanufacturer',
     ),
     'settings' => array(
     ),
@@ -39,10 +38,10 @@ $aModule = array(
         array('template' => 'manufacturer_main.tpl', 'block'=>'admin_manufacturer_main_form', 'file'=>'admin_manufacturer_main_form.tpl'),
     ),
     'templates' => array(
-        'asy_mantext_main.tpl'  => 'asy_mantext/out/admin/tpl/asy_mantext_main.tpl',
+        'asy_mantext_main.tpl'  => 'asy/asy_mantext/views/admin/tpl/asy_mantext_main.tpl',
     ),
-        'files' => array(
-        'asy_mantext_main'      => 'asy_mantext/admin/asy_mantext_main.php',
+    'files' => array(
+        'asy_mantext_main'      => 'asy/asy_mantext/controllers/admin/asy_mantext_main.php',
     ),
 
 
